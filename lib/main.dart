da3a6d364d2f45119256b3969a7d7b0e25928c29
yokartiklebron <berkersaptas@gmail.com>
2021-12-17 12:05:13 +0300

    düzenleme yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,23 @@ import 'package:blue_ajans/projeTakip.dart';
 import 'package:blue_ajans/tasarim.dart';
 import 'package:blue_ajans/teklifIsteFormu.dart';
 import 'package:blue_ajans/teknikDestek.dart';
+import 'package:blue_ajans/yapimAsamasinda.dart';
 import 'package:blue_ajans/yazilim.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Home()
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
 }
-
-
-
-
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -33,114 +28,160 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height:450,
-                child: Stack(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height/3,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/arkaPlan.jpeg"),
-                          fit: BoxFit.cover,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 450,
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height / 3,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/arkaPlan.jpeg"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(40.0),
+                          child: Image.asset("assets/logo.png"),
                         ),
                       ),
-                      child:Padding(
-                        padding: const EdgeInsets.all(40.0),
-                        child: Image.asset("assets/logo.png"),
-                      ),
-
+                      Positioned(
+                        top: 250,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              TextButton(
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Color(0x54000000),
+                                          spreadRadius: 4,
+                                          blurRadius: 20,
+                                        ),
+                                      ],
+                                    ),
+                                    child: ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(20.0),
+                                        child: Image.asset(
+                                          "assets/tasarim.jpeg",
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width /
+                                              2.2,
+                                        ))),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Tasarim()),
+                                  );
+                                },
+                              ),
+                              TextButton(
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Color(0x54000000),
+                                          spreadRadius: 4,
+                                          blurRadius: 20,
+                                        ),
+                                      ],
+                                    ),
+                                    child: ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(20.0),
+                                        child: Image.asset(
+                                          "assets/yazilim.jpeg",
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width /
+                                              2.2,
+                                        ))),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Yazilim()),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    TextButton(
+                      child: buildSecenek(context, "teknikDestek"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TeknikDestek()),
+                        );
+                      },
                     ),
-                    Positioned(top:250,child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          TextButton(
-                            child: Container(decoration:BoxDecoration(boxShadow: [
-                              BoxShadow(
-                                color: Color(0x54000000),
-                                spreadRadius:4,
-                                blurRadius: 20,
-                              ),
-                            ],),child: ClipRRect(borderRadius: BorderRadius.circular(20.0),child: Image.asset("assets/tasarim.jpeg",width:MediaQuery.of(context).size.width/2.2,))),
-                            onPressed: (){
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => Tasarim()),
-                              );
-                            },
-                          ),
-                          TextButton(
-                            child: Container(decoration:BoxDecoration(boxShadow: [
-                              BoxShadow(
-                                color: Color(0x54000000),
-                                spreadRadius:4,
-                                blurRadius: 20,
-                              ),
-                            ],), child: ClipRRect(borderRadius: BorderRadius.circular(20.0),child: Image.asset("assets/yazilim.jpeg",width:MediaQuery.of(context).size.width/2.2,))),
-                            onPressed: (){
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => Yazilim()),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                    ),)
+                    TextButton(
+                      child: buildSecenek(context, "projeTakip"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProjeTakipForm()),
+                        );
+                      },
+                    ),
+                    TextButton(
+                      child: buildSecenek(context, "teklifIsteyin"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TeklifIsteForm()),
+                        );
+                      },
+                    ),
+                    TextButton(
+                      child: buildSecenek(context, "guncelFiyatlar"),
+                      onPressed: () async {
+                        final url =
+                            'https://www.blueajans.com.tr/view/katalog.pdf';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        }
+                      },
+                    ),
+                    TextButton(
+                      child: buildSecenek(context, "onlineTahsilat"),
+                      onPressed: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              backgroundColor: Colors.blue,
+                              duration: Duration(seconds: 4),
+                              content: Text('Hizmet Yapım Aşamasındadır...')),
+                        );
+                      },
+                    ),
                   ],
                 ),
-              ),
-
-
-              Column(
-                children: [
-                  TextButton(child: buildSecenek(context,"teknikDestek"),onPressed: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TeknikDestek()),
-                    );
-                  },),
-                  TextButton(child: buildSecenek(context,"projeTakip"),onPressed: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ProjeTakipForm()),
-                    );
-                  },),
-                  TextButton(child: buildSecenek(context,"teklifIsteyin"),onPressed: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TeklifIsteForm()),
-                    );
-                  },),
-                  TextButton(child: buildSecenek(context,"guncelFiyatlar"),onPressed: ()async{
-                    final url = 'https://www.blueajans.com.tr/view/katalog.pdf';
-                    if(await canLaunch(url)){
-                      await launch(url);
-                    }
-
-                  },),
-                  TextButton(child: buildSecenek(context,"onlineTahsilat"),onPressed: (){},),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-      floatingActionButton:FloatActionButton()
-    );
+        floatingActionButton: FloatActionButton());
   }
-
-
-
-
-
 }
-
---
+++ lib/yapimAsamasinda.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class YapimAsamasinda extends StatelessWidget {
+  const YapimAsamasinda({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Yapim Aşamasindadır.'),
+    );
+  }
+}
